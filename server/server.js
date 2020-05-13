af02cc6ec52a2779617ff016219a0723c303d4f3
ash-sp-cole <sk8er9238@hotmail.com>
2020-05-13 13:22:46 +0200

    server express set up

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen( PORT , () => console.log("  server is listening at http://localhost => at PORT ", PORT));
+
--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash'
 import React, { Component, Fragment } from 'react'
-import { Popup, Button, Card, Divider, Header, Icon, Image, Placeholder } from 'semantic-ui-react'
+import { Feed, Grid, Button, Card, Divider, Header, Icon, Image, Placeholder } from 'semantic-ui-react'
 
 const cards = [
   {
@@ -38,13 +38,13 @@ export default class PlaceholderExampleCard extends Component {
     const { loading } = this.state
 
     return (
-      <Fragment style={{width:'60vw', opacity:'0.9' ,backgroundColor:'#12343b'}} id="cardMainDisplay">
+      <Fragment style={{margin:'auto', opacity:'0.9' ,backgroundColor:'#12343b'}} id="cardMainDisplay">
         {/* <Button loading={loading} onClick={this.handleLoadingClick} primary>
           refresh
         </Button> */}
-        
-   
-        <Card.Group doubling itemsPerRow={3} stackable style={{width:'40vw', textAlign:'center', background:'#2d545e' ,borderRadius: '25px', margin:'auto', marginBottom:'10px'}}>
+        <Grid columns={2} relaxed='very' stackable style={{margin:'auto', width:'80vw'}}>
+        <Grid.Column>
+        <Card.Group doubling itemsPerRow={3} stackable style={{ textAlign:'center', background:'#2d545e' ,borderRadius: '25px', marginBottom:'10px'}}>
           {_.map(cards, (card) => (
             <Card key={card.header}>
               {loading ? (
@@ -86,6 +86,95 @@ export default class PlaceholderExampleCard extends Component {
             </Card>
           ))}
         </Card.Group>
+        </Grid.Column>
+
+
+        <Grid.Column>
+        <Feed style={{backgroundColor:'white'}}>
+    <Feed.Event>
+      <Feed.Label>
+        <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+      </Feed.Label>
+      <Feed.Content>
+        <Feed.Summary>
+          <Feed.User>Elliot Fu</Feed.User> added you as a friend
+          <Feed.Date>1 Hour Ago</Feed.Date>
+        </Feed.Summary>
+        <Feed.Meta>
+          <Feed.Like>
+            <Icon name='like' />4 Likes
+          </Feed.Like>
+        </Feed.Meta>
+      </Feed.Content>
+    </Feed.Event>
+
+    <Feed.Event>
+      <Feed.Label image='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
+      <Feed.Content>
+        <Feed.Summary>
+          <a>Helen Troy</a> added <a>2 new illustrations</a>
+          <Feed.Date>4 days ago</Feed.Date>
+        </Feed.Summary>
+        <Feed.Extra images>
+          <a>
+            <img src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          </a>
+          <a>
+            <img src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          </a>
+        </Feed.Extra>
+        <Feed.Meta>
+          <Feed.Like>
+            <Icon name='like' />1 Like
+          </Feed.Like>
+        </Feed.Meta>
+      </Feed.Content>
+    </Feed.Event>
+
+    <Feed.Event>
+      <Feed.Label image='https://react.semantic-ui.com/images/avatar/small/jenny.jpg' />
+      <Feed.Content>
+        <Feed.Summary
+          date='2 Days Ago'
+          user='Jenny Hess'
+          content='add you as a friend'
+        />
+        <Feed.Meta>
+          <Feed.Like>
+            <Icon name='like' />8 Likes
+          </Feed.Like>
+        </Feed.Meta>
+      </Feed.Content>
+    </Feed.Event>
+
+    <Feed.Event>
+      <Feed.Label image='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
+      <Feed.Content>
+        <Feed.Summary>
+          <a>Joe Henderson</a> posted on his page
+          <Feed.Date>3 days ago</Feed.Date>
+        </Feed.Summary>
+        <Feed.Extra text>
+          Ours is a life of constant reruns. We're always circling back to where
+          we'd we started, then starting all over again. Even if we don't run
+          extra laps that day, we surely will come back for more of the same
+          another day soon.
+        </Feed.Extra>
+        <Feed.Meta>
+          <Feed.Like>
+            <Icon name='like' />5 Likes
+          </Feed.Like>
+        </Feed.Meta>
+      </Feed.Content>
+    </Feed.Event>
+
+    
+    
+ 
+  </Feed>
+
+</Grid.Column>
+</Grid>
       </Fragment>
       
     )
